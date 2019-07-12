25fd975caa467b0791c3cda5bece9f40fedc0c6b
desaichirayu <chirayu36@gmail.com>
2019-07-11 20:23:14 -0400

    Completed Course Grid

--- src/app/course-grid/course-grid.component.ts
+++ src/app/course-grid/course-grid.component.ts
@@ -4,17 +4,17 @@ import {CourseService} from '../services/course.service';
 @Component({
   selector: 'app-course-grid',
   templateUrl: './course-grid.component.html',
-  styleUrls: ['./course-grid.component.css']
+  styleUrls: ['./course-grid.component.scss']
 })
+
 export class CourseGridComponent implements OnInit {
 
   courses = [];
-  constructor(service: CourseService) {
-    service.findAllCourses()
-      .then(courses => this.courses = courses);
-  }
+  constructor(private service: CourseService) { }
 
   ngOnInit() {
+    this.service.findAllCourses()
+      .then(courses => this.courses = courses);
   }
 
 }
