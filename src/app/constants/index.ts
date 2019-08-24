d3176b6a9d0f12b83b5235b0b25ee8c78c2babd3
desaichirayu <chirayu36@gmail.com>
2019-08-24 02:59:26 -0400

    changed server

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -1,2 +1,2 @@
 // export const SERVER_URL = 'http://localhost:8080';
-export const SERVER_URL = 'https://wbdv-su-19-java-server-cdesai.herokuapp.com';
+export const SERVER_URL = 'https://whiteboard-server-spring.herokuapp.com';
